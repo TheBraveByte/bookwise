3c95947a34d08992a5f1446a0bc407026c76da31
dev-ayaa <ayaaakinleye@gmail.com>
2022-10-20 11:24:01 +0100

    update repo interface

--- query/queryrepo.go
+++ query/queryrepo.go
@@ -1,4 +1,10 @@
 package repo
 
+import (
+	"github.com/yusuf/p-catalogue/pkg/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type CatalogueRepo interface {
+	CreateUserAccount(user model.User) (int, primitive.ObjectID, error) 
 }
