a488bda2629d2a5a4a1395f6ba2b505fdef75222
dev-ayaa <ayaaakinleye@gmail.com>
2022-10-20 01:29:47 +0100

    setup routes

--- web/cmd/routes.go
+++ web/cmd/routes.go
@@ -1,7 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"time"
 
-func Router() http.Handler {
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/yusuf/p-catalogue/pkg/config"
+)
 
+func Route(app *config.CatalogueConfig) http.Handler {
+	
+	mux := chi.NewRouter()
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.RealIP)
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Timeout(100 * time.Second))
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods : []string{"GET", "POST", "PUT", "DELETE","OPTIONS"},
+		AllowedHeaders : []string{"Authorization", "Accept", "Content-Type"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: true,
+		MaxAge: 300,
+	}))
+	
+	return mux
+	
 }
