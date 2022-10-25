7e42ac2466657da7ba45647df9d4aa684c065a76
dev-ayaa <ayaaakinleye@gmail.com>
2022-10-25 21:28:37 +0100

    added session type to register

--- web/cmd/main.go
+++ web/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/gob"
 	"log"
 	"net/http"
 	"os"
@@ -12,6 +13,7 @@ import (
 	"github.com/yusuf/p-catalogue/pkg/config"
 	"github.com/yusuf/p-catalogue/pkg/controller"
 	"github.com/yusuf/p-catalogue/pkg/database"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var (
@@ -20,6 +22,10 @@ var (
 )
 
 func main() {
+	gob.Register(map[string]interface{}{})
+	gob.Register(map[string]string{})
+	gob.Register(primitive.NewObjectID())
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("no environment variable file")
@@ -32,17 +38,20 @@ func main() {
 	session.Cookie.Secure = true
 	session.Cookie.HttpOnly = true
 
-
 	log.Println("Starting p-catalogue API application server ...............")
 
 	uri := os.Getenv("mongodb_uri")
 	client := database.DatabaseConnection(uri)
+
+	log.Println("....Application connected to the database.......")
+	
 	defer func() {
 		err := client.Disconnect(context.TODO())
 		if err != nil {
 			log.Panic(err)
 		}
 	}()
+	app.Session = session
 
 	catalog := controller.NewCatalogue(&app, client)
 	controller.NewController(catalog)
