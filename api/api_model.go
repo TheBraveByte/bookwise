30bcfa3b4a732e0bef72122837362d23c7aba870
dev-ayaa <ayaaakinleye@gmail.com>
2022-11-10 22:32:08 +0100

    create model for incoming API response data

--- api/api_model.go
+++ api/api_model.go
@@ -1,11 +1,15 @@
 package api
 
 type Book struct {
-	AuthorName   string   `json:"author_name"`
-	Title        string   `json:"title,omitempty"`
+	AuthorName   []string `json:"author_name"`
+	Title        string   `json:"title"`
 	PublishYear  int      `json:"first_publish_year"`
 	Price        int      `json:"price"`
 	EditionCount int      `json:"edition_count"`
 	Language     []string `json:"language"`
-	Contributor  string   `json:"contributor"`
+	Contributor  []string `json:"contributor"`
+}
+
+type Docs struct {
+	Docs []Book `json:"docs"`
 }
