82429442edef9f7cd4be22bf8b8c28194868a046
dev-ayaa <ayaaakinleye@gmail.com>
2022-11-10 22:31:09 +0100

    restructure app model for user

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -6,22 +6,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Book struct {
-	ID       string `json:"id"`
-	Author   Author `json:"author"`
-	Title    string `json:"title,omitempty"`
-	Category string `json:"category"`
-	ISBN     string `json:"ISBN"`
-	Editor   string `json:"editor"`
-	Price    string `json:"price"`
-	Language string `json:"language"`
-}
-
-type Author struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-}
-
 type User struct {
 	ID         primitive.ObjectID `bson:"_id" json:"_id"`
 	FirstName  string             `json:"first_name" Usage:"required,alpha"`
@@ -35,7 +19,13 @@ type User struct {
 	RenewToken string             `json:"renew_token" Usage:"jwt"`
 }
 
-type ResponseMessage struct{
-	StatusCode int `json:"statuscode"`
-	Message string `json:"message"`
+type ResponseMessage struct {
+	StatusCode int    `json:"status_code"`
+	Message    string `json:"message"`
+}
+
+type Data struct {
+	Email    string
+	ID       primitive.ObjectID
+	Password string
 }
