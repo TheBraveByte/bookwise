d220c94bb8bd8318a89c08641dffd4827b1324da
dev-ayaa <ayaaakinleye@gmail.com>
2022-11-12 12:23:32 +0100

    initialize book collection in db

--- query/repo/dbcol.go
+++ query/repo/dbcol.go
@@ -3,6 +3,11 @@ package query
 import "go.mongodb.org/mongo-driver/mongo"
 
 func UserData(dbClient *mongo.Client, _ string) *mongo.Collection {
-	var userData = dbClient.Database("p_catalogue").Collection("user")
+	var userData = dbClient.Database("p_catalogue").Collection("controller")
 	return userData
+}
+
+func BookData(db *mongo.Client, collection string) *mongo.Collection {
+	var bookCollection = db.Database("p_catalogue").Collection("book")
+	return bookCollection
 }
