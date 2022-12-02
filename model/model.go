dd032a816a28fc02950f9d0bcbe1598bce31a9b8
dev-ayaa <ayaaakinleye@gmail.com>
2022-12-02 13:40:41 +0100

    library model added and collection

--- model/model.go
+++ model/model.go
@@ -1,24 +1,35 @@
 package model
 
 import (
-	"time"
-
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 )
 
+// User : model for users
 type User struct {
-	ID         primitive.ObjectID `bson:"_id" json:"_id"`
-	FirstName  string             `json:"first_name" Usage:"required,alpha"`
-	LastName   string             `json:"last_name" Usage:"required,alpha"`
-	Email      string             `json:"email" Usage:"required,alphanumeric"`
-	Password   string             `json:"password" Usage:"required,min=8,max=20"`
-	Catalogue  map[string]string  `json:"catalogue"`
-	CreatedAt  time.Time          `json:"created_at" Usage:"datetime=2006-01-02"`
-	UpdatedAt  time.Time          `json:"updated_at" Usage:"datetime=2006-01-02"`
-	Token      string             `json:"token" Usage:"jwt"`
-	RenewToken string             `json:"renew_token" Usage:"jwt"`
+	ID          primitive.ObjectID `bson:"_id" json:"_id"`
+	FirstName   string             `json:"first_name" Usage:"required,alpha"`
+	LastName    string             `json:"last_name" Usage:"required,alpha"`
+	Email       string             `json:"email" Usage:"required,alphanumeric"`
+	Password    string             `json:"password" Usage:"required,min=8,max=20"`
+	UserLibrary []UserLibrary      `json:"user_library"`
+	Token       string             `json:"token" Usage:"jwt"`
+	RenewToken  string             `json:"renew_token" Usage:"jwt"`
+	CreatedAt   time.Time          `json:"created_at" Usage:"datetime=2006-01-02"`
+	UpdatedAt   time.Time          `json:"updated_at" Usage:"datetime=2006-01-02"`
 }
 
+type UserLibrary struct {
+	AuthorName []string `json:"author_name"`
+	Title      string   `json:"title"`
+}
+
+type Library struct {
+	ID   primitive.ObjectID `bson:"_id" json:"_id"`
+	Book []Book             `json:"book" bson:"book"`
+}
+
+// Book sub-model for book API result
 type Book struct {
 	AuthorName   []string `json:"author_name"`
 	Title        string   `json:"title"`
@@ -29,15 +40,18 @@ type Book struct {
 	Contributor  []string `json:"contributor"`
 }
 
+// Docs : model for the API response when book is search by the user
 type Docs struct {
 	Docs []Book `json:"docs"`
 }
 
+// ResponseMessage : json response message for endpoints
 type ResponseMessage struct {
-	StatusCode int    `json:"status_code"`
-	Message    string `json:"message"`
+	StatusCode int `json:"status_code"`
+	Message    any `json:"message"`
 }
 
+// Data : model to store cookies data in sessions
 type Data struct {
 	Email    string
 	ID       primitive.ObjectID
--- modules/query/repo/dbcol.go
+++ modules/query/repo/dbcol.go
@@ -3,11 +3,11 @@ package query
 import "go.mongodb.org/mongo-driver/mongo"
 
 func UserData(dbClient *mongo.Client, _ string) *mongo.Collection {
-	var userData = dbClient.Database("p_catalogue").Collection("controller")
+	var userData = dbClient.Database("p_catalogue").Collection("user")
 	return userData
 }
 
-func BookData(db *mongo.Client, collection string) *mongo.Collection {
-	var bookCollection = db.Database("p_catalogue").Collection("book")
-	return bookCollection
+func LibraryData(db *mongo.Client, collection string) *mongo.Collection {
+	var library = db.Database("p_catalogue").Collection("library")
+	return library
 }
