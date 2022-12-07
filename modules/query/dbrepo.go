fda97072d1d645ab586ae56fa0459c0dafc4762c
dev-ayaa <ayaaakinleye@gmail.com>
2022-12-07 16:02:29 +0100

    added new queries methods to interface

--- modules/query/dbrepo.go
+++ modules/query/dbrepo.go
@@ -6,13 +6,15 @@ import (
 )
 
 type CatalogueRepo interface {
-	SendAvailableBooks() ([]model.Library, error)
+	SendAvailableBooks() ([]primitive.M, error)
+
 	CreateUserAccount(user model.User) (int, primitive.ObjectID, error)
 	VerifyUser(email, password, hashedPassword string) (bool, error)
 	UpdateUserDetails(userID primitive.ObjectID, token, renewToken string) error
-	UpdateUserBook(userID primitive.ObjectID, book model.Library) error
-	GetUserBooks(userID primitive.ObjectID) ([]model.UserLibrary, error)
-	FindBook(userID primitive.ObjectID, title string) (model.UserLibrary, error)
+
+	UpdateUserBook(userID, bookId primitive.ObjectID, bookData primitive.M) error
+	GetUserBooks(userID primitive.ObjectID) (primitive.A, error)
+	FindBook(userID primitive.ObjectID, title string) (primitive.M, error)
 	DeleteBook(bookID, userID primitive.ObjectID) error
 
 	CheckLibrary(title string, bookData model.Book) (int64, primitive.ObjectID, error)
